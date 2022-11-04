810ec234b3a762c872a801684aad713bdc5e6105
Icedude907 <34080011+Icedude907@users.noreply.github.com>
2022-11-05 11:55:12 +1300

    C Cleanup.

--- mods/cpp_mod/main.cpp
+++ mods/cpp_mod/main.cpp
@@ -1,9 +1,6 @@
-#include <stdint.h>
-
-#include "./binding/binding.h"
+// #include <stdint.h>
 
 // Ok these bindings are definitely for C. No C++ autocasting or namespacing nice things here.
 void modmain(){
-    binding_string_t b; binding_string_set(&b, "Hello from the mod!");
-    binding_print_chat(&b);
+
 }
